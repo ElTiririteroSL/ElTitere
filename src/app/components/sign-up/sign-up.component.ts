6557a1cd6bb84e49df22c17cc9913fd4a2403dfd
DirtdiverIV <davidgomez.seg@gmail.com>
2023-12-02 01:46:06 +0100

    Added verification fields to register form

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -9,7 +9,33 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 export class SignUpComponent {
   constructor(private authService: AuthService) {}
 
-  signUp(email: string, password: string, username: string) {
+  signUp(email: string, confirmEmail: string, password: string, confirmPassword: string, username: string) {
+    if (email !== confirmEmail) {
+      alert('Email and Confirm Email must match.');
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      alert('Password and Confirm Password must match.');
+      return;
+    }
+
+    if (!username) {
+      alert('Username cannot be empty.');
+      return;
+    }
+
+    if (!this.isValidEmail(email)) {
+      alert('Invalid email format.');
+      return;
+    }
+
     this.authService.signUpWithEmailAndPassword(email, password, username);
+  }
+
+  private isValidEmail(email: string): boolean {
+    // Expresión regular para validar el formato de una dirección de correo electrónico
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
 }
