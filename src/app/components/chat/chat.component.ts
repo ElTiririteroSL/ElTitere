b3766e64440b9e75c52c7d64f74df9e34f881be8
DirtdiverIV <davidgomez.seg@gmail.com>
2023-12-05 13:06:33 +0100

    Added scroll to chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ChatService } from 'src/app/shared/services/chat.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
@@ -12,22 +12,35 @@ export class ChatComponent implements OnInit {
   newMessage: string = '';
   username: string = 'Usuario Anónimo';
 
+  @ViewChild('messagesDiv') messagesDiv: ElementRef | undefined;
+
   constructor(private chatService: ChatService, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.chatService.getMessages().subscribe((messages) => {
       this.messages = messages;
+      this.scrollToBottom();
     });
   }
 
   sendMessage(): void {
     if (this.newMessage.trim() !== '') {
-      // Obtén el nombre de usuario justo antes de enviar el mensaje
       this.authService.getCurrentUsername().subscribe((username: string) => {
         this.username = username || 'Usuario Anónimo';
         this.chatService.sendMessage(this.newMessage, this.username);
         this.newMessage = '';
+
+        // Esperar un breve momento antes de desplazarse hacia abajo
+        setTimeout(() => {
+          this.scrollToBottom();
+        }, 100);
       });
+    }
+  }
+
+  private scrollToBottom(): void {
+    if (this.messagesDiv) {
+      this.messagesDiv.nativeElement.scrollTop = this.messagesDiv.nativeElement.scrollHeight;
     }
   }
 }
